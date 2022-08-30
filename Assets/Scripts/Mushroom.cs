ef492cdabb62502b247b967d2e4407a0bffd94b3
fahsaaulanh <fahsaaula8@gmail.com>
2022-08-31 05:39:41 +0700

    update

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -6,14 +6,24 @@ public class Mushroom : MonoBehaviour
 {
     public float upForce = 1f;
     public float sideForce = .1f;
-    void Start()
+
+    private int _timer = 3;
+
+    private void OnEnable()
     {
         float xForce = Random.Range(-sideForce, sideForce);
-        float yForce = Random.Range(upForce/2f, upForce);
+        float yForce = Random.Range(upForce / 2f, upForce);
         float zForce = Random.Range(-sideForce, sideForce);
 
-        Vector3 force = new Vector3 (xForce, yForce, zForce);
-
+        Vector3 force = new Vector3(xForce, yForce, zForce);
         GetComponent<Rigidbody>().velocity = force;
+
+        //StartCoroutine(Countdown());
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(3);
+        gameObject.SetActive(false);
     }
 }
